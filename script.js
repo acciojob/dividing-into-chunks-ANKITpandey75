bfbd1896bd15a69cb5093942db155565f2da73f8
ANKITpandey75 <125044396+ANKITpandey75@users.noreply.github.com>
2026-06-20 06:10:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
+	  n = Number(n);
+
+  let result = [];
+  let chunk = [];
+  let sum = 0;
+
+  for (let num of arr) {
+    if (sum + num <= n) {
+      chunk.push(num);
+      sum += num;
+    } else {
+      result.push(chunk);
+      chunk = [num];
+      sum = num;
+    }
+  }
+
+  if (chunk.length > 0) {
+    result.push(chunk);
+  }
+
+  return result;
   // Write your code here
 };
 
